471a8b809344c21df729f09e0cdc5c748d33fb9d
Ronnie Dutta <binaryfunt@users.noreply.github.com>
2019-08-02 18:08:29 +0100

    Remove quick-fold-view

--- spec/quick-fold-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import QuickFoldView from '../lib/quick-fold-view';
-
-describe('QuickFoldView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
