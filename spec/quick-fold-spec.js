b71dcc75e4a8b44a967dc68d37d2bc6aa0acec12
Ronnie Dutta <binaryfunt@users.noreply.github.com>
2019-10-17 02:43:12 +0100

    Get package activation in specs working

--- spec/quick-fold-spec.js
+++ spec/quick-fold-spec.js
@@ -1,40 +1,35 @@
-'use babel';
-
-import QuickFold from '../lib/quick-fold';
-
 describe('QuickFold package', () => {
 
     let editor, editorView, activationPromise;
+
+    const foldNext = async () => {
+        atom.commands.dispatch(editorView, 'quick-fold:fold-next');
+        await activationPromise;
+    };
 
     beforeEach(async () => {
         await atom.packages.activatePackage('language-javascript');
         await atom.workspace.open('sample.js');
 
-        editor = atom.workspace.getActiveTextEditor();
-        editorView = atom.views.getView(editor);
+        runs(() => {
+            editor = atom.workspace.getActiveTextEditor();
+            editorView = atom.views.getView(editor);
+
+            return activationPromise = atom.packages.activatePackage('quick-fold');
+        });
     });
 
     describe('when the specs are run', () => {
-        it('opens the sample file', () => {
-            expect(!editor.isEmpty()).toBe(true);
-        });
+        it('opens the sample file', () => expect(!editor.isEmpty()).toBe(true));
     });
 
     describe('when the quick-fold:fold-next event is triggered', () => {
-        beforeEach(async () => {
-            atom.commands.dispatch(editorView, 'quick-fold:fold-next');
-            await atom.packages.activatePackage('quick-fold');
-        });
-
         it('activates the package', () => {
-            atom.commands.dispatch(editorView, 'quick-fold:fold-next');
-            expect(atom.packages.isPackageActive('quick-fold')).toBe(true);
+            return foldNext().then(() => expect(atom.packages.isPackageActive('quick-fold')).toBe(true));
         });
 
         it('moves the cursor to a different line number', () => {
-            atom.commands.dispatch(editorView, 'quick-fold:fold-next');
-            console.log(atom.packages.isPackageActive('quick-fold'));
-            expect(editor.getCursorScreenPosition().row).not.toBe(0);
+            return foldNext().then(() => expect(editor.getCursorScreenPosition().row).not.toBe(0));
         });
     });
 });
